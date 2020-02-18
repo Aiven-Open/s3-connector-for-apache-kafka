ebb0ff576fcfdbd5aa6518e471ae0182d7cd2e78
Andrey Pleskach <ples@aiven.io>
2020-02-18 15:35:08 +0100

    reformat tests code according code guidelines

--- src/test/java/io/aiven/kafka/connect/s3/AivenKafkaConnectS3MultipartUploadTest.java
+++ src/test/java/io/aiven/kafka/connect/s3/AivenKafkaConnectS3MultipartUploadTest.java
@@ -39,43 +39,42 @@ public class AivenKafkaConnectS3MultipartUploadTest {
 
     @Test
     public void testAivenKafkaConnectS3MultipartUploadTest() throws IOException {
-        Random generator = new Random();
-        int port = generator.nextInt(10000) + 10000;
+        final Random generator = new Random();
+        final int port = generator.nextInt(10000) + 10000;
 
-        S3Mock api = new S3Mock.Builder().withPort(port).withInMemoryBackend().build();
+        final S3Mock api = new S3Mock.Builder().withPort(port).withInMemoryBackend().build();
         api.start();
 
-        BasicAWSCredentials awsCreds = new BasicAWSCredentials(
+        final BasicAWSCredentials awsCreds = new BasicAWSCredentials(
             "test_key_id",
             "test_secret_key"
         );
 
-        AmazonS3ClientBuilder builder = AmazonS3ClientBuilder.standard();
+        final AmazonS3ClientBuilder builder = AmazonS3ClientBuilder.standard();
         builder.withCredentials(new AWSStaticCredentialsProvider(awsCreds));
         builder.withEndpointConfiguration(new EndpointConfiguration("http://localhost:" + port, "us-west-2"));
         builder.withPathStyleAccessEnabled(true);
 
-        AmazonS3 s3Client = builder.build();
-
+        final AmazonS3 s3Client = builder.build();
         s3Client.createBucket("test-bucket");
 
-        AivenKafkaConnectS3MultipartUpload mp = new AivenKafkaConnectS3MultipartUpload(
+        final AivenKafkaConnectS3MultipartUpload mp = new AivenKafkaConnectS3MultipartUpload(
             s3Client,
             "test-bucket",
             "test-object"
         );
 
-        byte[] data = "foobar".getBytes();
-        InputStream stream = new ByteArrayInputStream(data, 0, data.length);
+        final byte[] data = "foobar".getBytes();
+        final InputStream stream = new ByteArrayInputStream(data, 0, data.length);
         mp.uploadPart(stream, data.length);
         mp.commit();
 
-        S3Object object = s3Client.getObject(new GetObjectRequest("test-bucket", "test-object"));
-        InputStream objectData = object.getObjectContent();
+        final S3Object object = s3Client.getObject(new GetObjectRequest("test-bucket", "test-object"));
+        final InputStream objectData = object.getObjectContent();
         assertEquals(objectData.available(), 6);
-        byte[] stored_data = new byte[data.length];
-        objectData.read(stored_data, 0, data.length);
-        assertArrayEquals(data, stored_data);
+        final byte[] storedData = new byte[data.length];
+        objectData.read(storedData, 0, data.length);
+        assertArrayEquals(data, storedData);
         objectData.close();
 
         api.stop();
--- src/test/java/io/aiven/kafka/connect/s3/AivenKafkaConnectS3OutputStreamTest.java
+++ src/test/java/io/aiven/kafka/connect/s3/AivenKafkaConnectS3OutputStreamTest.java
@@ -35,29 +35,29 @@ public class AivenKafkaConnectS3OutputStreamTest {
 
     @Test
     public void testAivenKafkaConnectS3OutputStreamTest() throws IOException {
-        Random generator = new Random();
-        int port = generator.nextInt(10000) + 10000;
+        final Random generator = new Random();
+        final int port = generator.nextInt(10000) + 10000;
 
-        S3Mock api = new S3Mock.Builder().withPort(port).withInMemoryBackend().build();
+        final S3Mock api = new S3Mock.Builder().withPort(port).withInMemoryBackend().build();
         api.start();
 
-        BasicAWSCredentials awsCreds = new BasicAWSCredentials(
+        final BasicAWSCredentials awsCreds = new BasicAWSCredentials(
             "test_key_id",
             "test_secret_key"
         );
 
-        AmazonS3ClientBuilder builder = AmazonS3ClientBuilder.standard();
+        final AmazonS3ClientBuilder builder = AmazonS3ClientBuilder.standard();
         builder.withCredentials(new AWSStaticCredentialsProvider(awsCreds));
         builder.withEndpointConfiguration(new EndpointConfiguration("http://localhost:" + port, "us-west-2"));
         builder.withPathStyleAccessEnabled(true);
 
-        AmazonS3 s3Client = builder.build();
-
+        final AmazonS3 s3Client = builder.build();
         s3Client.createBucket("test-bucket");
 
-        AivenKafkaConnectS3OutputStream storageSmall = new AivenKafkaConnectS3OutputStream(s3Client, "test-bucket", "test-key-small");
+        final AivenKafkaConnectS3OutputStream storageSmall =
+            new AivenKafkaConnectS3OutputStream(s3Client, "test-bucket", "test-key-small");
 
-        byte[] inputSmall = "small".getBytes();
+        final byte[] inputSmall = "small".getBytes();
         storageSmall.write(inputSmall);
         assertFalse(s3Client.doesObjectExist("test-bucket", "test-key-small"));
         storageSmall.flush();
@@ -65,8 +65,9 @@ public class AivenKafkaConnectS3OutputStreamTest {
         storageSmall.close();
         assertTrue(s3Client.doesObjectExist("test-bucket", "test-key-small"));
 
-        AivenKafkaConnectS3OutputStream storageLarge = new AivenKafkaConnectS3OutputStream(s3Client, "test-bucket", "test-key-large");
-        byte[] inputLarge = new byte[1024*1024*10];
+        final AivenKafkaConnectS3OutputStream storageLarge =
+            new AivenKafkaConnectS3OutputStream(s3Client, "test-bucket", "test-key-large");
+        final byte[] inputLarge = new byte[1024 * 1024 * 10];
         storageLarge.write(inputLarge);
         assertFalse(s3Client.doesObjectExist("test-bucket", "test-key-large"));
         storageLarge.flush();
