xjrk58/aiven-kafka-connect-s3

Task refactoring

src/main/java/io/aiven/kafka/connect/s3/S3SinkTask.java
@@ -1,0 +1,89 @@
+/*
+ * Copyright (C) 2020 Aiven Oy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.aiven.kafka.connect.s3;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Objects;
+
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.connect.errors.ConnectException;
+import org.apache.kafka.connect.sink.SinkRecord;
+import org.apache.kafka.connect.sink.SinkTask;
+
+import io.aiven.kafka.connect.common.Version;
+import io.aiven.kafka.connect.common.lang.Pair;
+import io.aiven.kafka.connect.s3.config.S3SyncConfig;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class S3SinkTask extends SinkTask {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(S3SinkConnector.class);
+
+    private S3SyncConfig config;
+
+    private S3StreamWriter streamMap;
+
+    // required by Connect
+    public S3SinkTask() {
+    }
+
+    @Override
+    public void start(final Map<String, String> props) {
+        Objects.requireNonNull(props, "props hasn't been set");
+        this.config = new S3SyncConfig(props);
+        this.streamMap = new S3StreamWriter(config);
+    }
+
+    @Override
+    public void put(final Collection<SinkRecord> records) throws ConnectException {
+        LOGGER.info("Processing {} records", records.size());
+        records.stream()
+            .map(r -> Pair.of(new TopicPartition(r.topic(), r.kafkaPartition()), r))
+            .forEach(p -> streamMap.write(p.left(), p.right()));
+    }
+
+    @Override
+    public void flush(final Map<TopicPartition, OffsetAndMetadata> offsets) {
+        offsets.keySet().forEach(streamMap::flush);
+    }
+
+    @Override
+    public void close(final Collection<TopicPartition> partitions) throws ConnectException {
+        partitions.forEach(tp -> {
+            LOGGER.info("Close {}", tp);
+            streamMap.close(tp);
+        });
+    }
+
+    @Override
+    public void stop() {
+        LOGGER.info("Stop  S3 Sink Task");
+        streamMap.closeAll();
+    }
+
+
+    @Override
+    public String version() {
+        return Version.getVersion(S3SyncConfig.VERSION_FILE);
+    }
+
+}

src/main/java/io/aiven/kafka/connect/s3/S3StreamWriter.java
@@ -1,0 +1,184 @@
+/*
+ * Copyright (C) 2020 Aiven Oy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.aiven.kafka.connect.s3;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.zip.GZIPOutputStream;
+
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.connect.errors.ConnectException;
+import org.apache.kafka.connect.sink.SinkRecord;
+
+import io.aiven.kafka.connect.common.config.CompressionType;
+import io.aiven.kafka.connect.common.config.Variables;
+import io.aiven.kafka.connect.common.output.OutputWriter;
+import io.aiven.kafka.connect.common.templating.Template;
+import io.aiven.kafka.connect.s3.config.S3SyncConfig;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static io.aiven.kafka.connect.common.config.FormatterUtils.formatKafkaOffset;
+import static io.aiven.kafka.connect.common.config.FormatterUtils.formatTimestamp;
+
+public class S3StreamWriter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(S3StreamWriter.class);
+
+    private final OutputWriter outputWriter;
+
+    private final Template prefixTemplate;
+
+    private final S3SyncConfig config;
+
+    private final AmazonS3 s3Client;
+
+    private final Map<TopicPartition, OutputStream> streams;
+
+    public S3StreamWriter(final S3SyncConfig config) {
+        super();
+        this.prefixTemplate = config.getPrefixTemplate();
+        this.config = config;
+        final var awsEndPOntConfig = newEndpointConfiguration(this.config);
+        final var s3ClientBuilder =
+            AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(
+                    new AWSStaticCredentialsProvider(
+                        new BasicAWSCredentials(
+                            config.getAwsAccessKeyId().value(),
+                            config.getAwsSecretKey().value()
+                        )
+                    )
+                );
+        if (Objects.isNull(awsEndPOntConfig)) {
+            s3ClientBuilder.withRegion(config.getRegion());
+        } else {
+            s3ClientBuilder
+                .withEndpointConfiguration(awsEndPOntConfig)
+                .withPathStyleAccessEnabled(true);
+        }
+        this.s3Client = s3ClientBuilder.build();
+        this.outputWriter =
+            OutputWriter
+                .builder()
+                .addFields(config.getOutputFields())
+                .build();
+        this.streams = new HashMap<>();
+    }
+
+    private AwsClientBuilder.EndpointConfiguration newEndpointConfiguration(final S3SyncConfig config) {
+        return Objects.nonNull(config.getAwsEndPoint())
+            ? new AwsClientBuilder.EndpointConfiguration(config.getAwsEndPoint(), config.getRegion().getName())
+            : null;
+    }
+
+    public void write(final TopicPartition topicPartition, final SinkRecord record) {
+        final var out = streams.computeIfAbsent(topicPartition, ignored -> newStreamFor(record));
+        try {
+            outputWriter.writeRecord(record, out);
+            out.flush();
+        } catch (final IOException e) {
+            throw new ConnectException(e);
+        }
+    }
+
+    private OutputStream newStreamFor(final SinkRecord record) {
+        final var prefix =
+            prefixTemplate
+                .instance()
+                .bindVariable(
+                    Variables.TIMESTAMP.name,
+                    tsParameter -> formatTimestamp.apply(config.getTimestampSource(), tsParameter)
+                )
+                .bindVariable(
+                    Variables.PARTITION.name,
+                    () -> record.kafkaPartition().toString()
+                )
+                .bindVariable(
+                    Variables.START_OFFSET.name,
+                    parameter -> formatKafkaOffset.apply(record, parameter)
+                )
+                .bindVariable(Variables.TOPIC.name, record::topic)
+                .bindVariable(
+                    "utc_date",
+                    () -> ZonedDateTime.now(ZoneId.of("UTC")).format(DateTimeFormatter.ISO_LOCAL_DATE)
+                )
+                .bindVariable(
+                    "local_date",
+                    () -> LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE)
+                )
+                .render();
+        final var key =
+            String.format(
+                "%s-%s-%s",
+                record.topic(),
+                record.kafkaPartition(),
+                formatKafkaOffset(record));
+        final var fullKey = config.getCompressionType() == CompressionType.GZIP ? prefix + key + ".gz" : prefix + key;
+        final var awsOutputStream = new S3OutputStream(s3Client, config.getBucketName(), fullKey);
+        try {
+            return config.getCompressionType() == CompressionType.GZIP
+                ? new GZIPOutputStream(awsOutputStream)
+                : awsOutputStream;
+        } catch (final IOException e) {
+            throw new ConnectException(e);
+        }
+    }
+
+    public void flush(final TopicPartition topicPartition) {
+        LOGGER.debug("Flush data for {}", topicPartition);
+        closeAndRemove(topicPartition);
+    }
+
+    public void close(final TopicPartition topicPartition) {
+        LOGGER.info("Close stream for {}", topicPartition);
+        closeAndRemove(topicPartition);
+    }
+
+    private void closeAndRemove(final TopicPartition topicPartition) {
+        try {
+            streams.getOrDefault(
+                topicPartition,
+                OutputStream.nullOutputStream()).close();
+            streams.remove(topicPartition);
+        } catch (final IOException e) {
+            throw new ConnectException(e);
+        }
+    }
+
+    public void closeAll() {
+        for (final Map.Entry<TopicPartition, OutputStream> e : streams.entrySet()) {
+            close(e.getKey());
+        }
+    }
+
+}

Aiven-Open/s3-connector-for-apache-kafka

src/main/java/io/aiven/kafka/connect/s3/S3SinkTask.java
@@ -1,0 +1,92 @@
+/*
+ * Copyright (C) 2020 Aiven Oy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.aiven.kafka.connect.s3;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Objects;
+
+import io.aiven.kafka.connect.commons.config.S3SinkConfig;
+import io.aiven.kafka.connect.commons.templating.Pair;
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.connect.errors.ConnectException;
+import org.apache.kafka.connect.sink.SinkRecord;
+import org.apache.kafka.connect.sink.SinkTask;
+
+import io.aiven.kafka.connect.common.Version;
+import io.aiven.kafka.connect.common.lang.Pair;
+import io.aiven.kafka.connect.s3.config.S3SyncConfig;
+
+import org.glassfish.jersey.internal.Version;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class S3SinkTask extends SinkTask {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(S3SinkConnector.class);
+
+    private S3SinkConfig config;
+
+    private S3StreamWriter streamMap;
+
+    // required by Connect
+    public S3SinkTask() {
+    }
+
+    @Override
+    public void start(final Map<String, String> props) {
+        Objects.requireNonNull(props, "props hasn't been set");
+        this.config = new S3SinkConfig(props);
+        this.streamMap = new S3StreamWriter(config);
+    }
+
+    @Override
+    public void put(final Collection<SinkRecord> records) throws ConnectException {
+        LOGGER.info("Processing {} records", records.size());
+        records.stream()
+            .map(r -> Pair.of(new TopicPartition(r.topic(), r.kafkaPartition()), r))
+            .forEach(p -> streamMap.write(p.left(), p.right()));
+    }
+
+    @Override
+    public void flush(final Map<TopicPartition, OffsetAndMetadata> offsets) {
+        offsets.keySet().forEach(streamMap::flush);
+    }
+
+    @Override
+    public void close(final Collection<TopicPartition> partitions) throws ConnectException {
+        partitions.forEach(tp -> {
+            LOGGER.info("Close {}", tp);
+            streamMap.close(tp);
+        });
+    }
+
+    @Override
+    public void stop() {
+        LOGGER.info("Stop  S3 Sink Task");
+        streamMap.closeAll();
+    }
+
+
+    @Override
+    public String version() {
+        return Version.getVersion(S3SinkConfig.VERSION_FILE);
+    }
+
+}

src/main/java/io/aiven/kafka/connect/s3/S3StreamWriter.java
@@ -1,0 +1,185 @@
+/*
+ * Copyright (C) 2020 Aiven Oy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.aiven.kafka.connect.s3;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.zip.GZIPOutputStream;
+
+import io.aiven.kafka.connect.commons.config.S3SinkConfig;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.connect.errors.ConnectException;
+import org.apache.kafka.connect.sink.SinkRecord;
+
+import io.aiven.kafka.connect.common.config.CompressionType;
+import io.aiven.kafka.connect.common.config.Variables;
+import io.aiven.kafka.connect.common.output.OutputWriter;
+import io.aiven.kafka.connect.common.templating.Template;
+import io.aiven.kafka.connect.s3.config.S3SyncConfig;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static io.aiven.kafka.connect.common.config.FormatterUtils.formatKafkaOffset;
+import static io.aiven.kafka.connect.common.config.FormatterUtils.formatTimestamp;
+
+public class S3StreamWriter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(S3StreamWriter.class);
+
+    private final OutputWriter outputWriter;
+
+    private final Template prefixTemplate;
+
+    private final S3SinkConfig config;
+
+    private final AmazonS3 s3Client;
+
+    private final Map<TopicPartition, OutputStream> streams;
+
+    public S3StreamWriter(final S3SinkConfig config) {
+        super();
+        this.prefixTemplate = config.getPrefixTemplate();
+        this.config = config;
+        final var awsEndPOntConfig = newEndpointConfiguration(this.config);
+        final var s3ClientBuilder =
+            AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(
+                    new AWSStaticCredentialsProvider(
+                        new BasicAWSCredentials(
+                            config.getAwsAccessKeyId().value(),
+                            config.getAwsSecretKey().value()
+                        )
+                    )
+                );
+        if (Objects.isNull(awsEndPOntConfig)) {
+            s3ClientBuilder.withRegion(config.getAwsS3Region());
+        } else {
+            s3ClientBuilder
+                .withEndpointConfiguration(awsEndPOntConfig)
+                .withPathStyleAccessEnabled(true);
+        }
+        this.s3Client = s3ClientBuilder.build();
+        this.outputWriter =
+            OutputWriter
+                .builder()
+                .addFields(config.getOutputFields())
+                .build();
+        this.streams = new HashMap<>();
+    }
+
+    private AwsClientBuilder.EndpointConfiguration newEndpointConfiguration(final S3SinkConfig config) {
+        return Objects.nonNull(config.getAwsS3EndPoint())
+            ? new AwsClientBuilder.EndpointConfiguration(config.getAwsS3EndPoint(), config.getAwsS3Region().getName())
+            : null;
+    }
+
+    public void write(final TopicPartition topicPartition, final SinkRecord record) {
+        final var out = streams.computeIfAbsent(topicPartition, ignored -> newStreamFor(record));
+        try {
+            outputWriter.writeRecord(record, out);
+            out.flush();
+        } catch (final IOException e) {
+            throw new ConnectException(e);
+        }
+    }
+
+    private OutputStream newStreamFor(final SinkRecord record) {
+        final var prefix =
+            prefixTemplate
+                .instance()
+                .bindVariable(
+                    Variables.TIMESTAMP.name,
+                    tsParameter -> formatTimestamp.apply(config.getTimestampSource(), tsParameter)
+                )
+                .bindVariable(
+                    Variables.PARTITION.name,
+                    () -> record.kafkaPartition().toString()
+                )
+                .bindVariable(
+                    Variables.START_OFFSET.name,
+                    parameter -> formatKafkaOffset.apply(record, parameter)
+                )
+                .bindVariable(Variables.TOPIC.name, record::topic)
+                .bindVariable(
+                    "utc_date",
+                    () -> ZonedDateTime.now(ZoneId.of("UTC")).format(DateTimeFormatter.ISO_LOCAL_DATE)
+                )
+                .bindVariable(
+                    "local_date",
+                    () -> LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE)
+                )
+                .render();
+        final var key =
+            String.format(
+                "%s-%s-%s",
+                record.topic(),
+                record.kafkaPartition(),
+                formatKafkaOffset(record));
+        final var fullKey = config.getCompressionType() == CompressionType.GZIP ? prefix + key + ".gz" : prefix + key;
+        final var awsOutputStream = new S3OutputStream(s3Client, config.getBucketName(), fullKey);
+        try {
+            return config.getCompressionType() == CompressionType.GZIP
+                ? new GZIPOutputStream(awsOutputStream)
+                : awsOutputStream;
+        } catch (final IOException e) {
+            throw new ConnectException(e);
+        }
+    }
+
+    public void flush(final TopicPartition topicPartition) {
+        LOGGER.debug("Flush data for {}", topicPartition);
+        closeAndRemove(topicPartition);
+    }
+
+    public void close(final TopicPartition topicPartition) {
+        LOGGER.info("Close stream for {}", topicPartition);
+        closeAndRemove(topicPartition);
+    }
+
+    private void closeAndRemove(final TopicPartition topicPartition) {
+        try {
+            streams.getOrDefault(
+                topicPartition,
+                OutputStream.nullOutputStream()).close();
+            streams.remove(topicPartition);
+        } catch (final IOException e) {
+            throw new ConnectException(e);
+        }
+    }
+
+    public void closeAll() {
+        for (final Map.Entry<TopicPartition, OutputStream> e : streams.entrySet()) {
+            close(e.getKey());
+        }
+    }
+
+}